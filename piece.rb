b864583430e5e250ce80fce1149b8bb09d768f0d
Sara LaFassett <sara@lafassett.com>
2014-01-23 14:55:11 -0800

    Piece classes built out for initialize and move methods. Still need place and rotate.

--- piece.rb
+++ piece.rb
@@ -1,14 +1,63 @@
+require "pry"
+
 class Piece
 
+  attr_reader :x_position, :positions, :current_rotation, :board_width, :max_x
 
+  def initialize(board_width)
+    @board_width = board_width
 
-  def initialize
+    # since indexes start at 0, let's save some trouble and start our x_position at 0
+    @x_position = 0
 
+    # the index of the current rotation position; stored to make it easier to rotate later
+    @current_rotation = 0
+  end
+
+  def move(direction)
+    # set our change to 1 if moving right, -1 if moving left
+    change = direction == "right" ? 1 : -1
+
+    new_x = @x_position + change
+
+    unless new_x < 0 || new_x > @max_x
+      @x_position = new_x
+    else
+      raise "You've reached the end of the board!"
+    end
+  end
+
+  def rotate
 
   end
 
-  def move_left
+  def update_max_x
+    # make sure we can't fall off the edge of the board
+    @max_x = @board_width - @positions[@current_rotation].length
+  end
 
+end
+
+class PieceI < Piece
+
+  def initialize(board_width)
+    super
+    @positions = [ ["XXXX"],
+                   ["X",
+                    "X",
+                    "X",
+                    "X"] ]
+  end
+
+end
+
+class PieceO < Piece
+
+  def initialize(board_width)
+    super
+    @positions = [ ["XX",
+                    "XX"] ]
+    update_max_x
   end
 
 end
--- spec/piece_spec.rb
+++ spec/piece_spec.rb
@@ -2,9 +2,84 @@ require "spec_helper"
 require_relative "../piece"
 
 describe Piece do
-  let(:piece) { Piece.new }
+  let(:width) { rand(4..20) }
+  let(:piece) { Piece.new(width) }
+
+  describe "initialize" do
+    it "should have an x coordinate" do
+      piece.should respond_to(:x_position)
+      piece.x_position == 0
+    end
+
+    it "should have an array of positions" do
+      piece.should respond_to(:positions)
+    end
+
+    it "should have a current position" do
+      piece.should respond_to(:current_rotation)
+    end
+
+    it "should know the board width" do
+      piece.should respond_to(:board_width)
+      piece.board_width.should == width
+    end
+  end
+
+end
+
+describe PieceO do
+  let(:width) { rand(4..20) }
+  let(:piece) { PieceO.new(width) }
+
+  describe "initialize" do
+    it "should have the same attributes as its super class" do
+      piece.should respond_to(:x_position)
+      piece.should respond_to(:positions)
+      piece.should respond_to(:current_rotation)
+      piece.should respond_to(:board_width)
+    end
+
+    it "should have 1 position" do
+      piece.positions.length.should == 1
+    end
+
+    it "should set the max_x" do
+      max_x = piece.board_width - piece.positions[piece.current_rotation].length
+      piece.max_x.should == max_x
+    end
+  end
+
+  describe "move" do
+    it "should move to the right" do
+      expect{piece.move("right")}.to change{piece.x_position}.by(1)
+    end
+
+    it "shouldn't move to the right past the max_x" do
+      piece.max_x.times { piece.move("right") }
+      expect{piece.move("right")}.to raise_error{RuntimeError}
+    end
+
+    it "should move to the left" do
+      piece.move("right")
+      expect{piece.move("left")}.to change{piece.x_position}.by(-1)
+    end
+
+    it "should not move to left past 0" do
+      expect{piece.move("left")}.to raise_error{RuntimeError}
+    end
+
+  end
 
 
+end
 
+describe PieceI do
+  let(:width) { rand(4..20) }
+  let(:piece) { PieceI.new(width) }
 
+  describe "initialize" do
+    it "should have 2 positions" do
+      piece.positions.length.should == 2
+    end
+  end
 end
